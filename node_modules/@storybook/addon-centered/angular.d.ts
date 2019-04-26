a16d7954f3ab4bea20fcd37913a596d68bdecb35
Upkar Singh <upkar.singh@zomato.com>
2019-04-26 17:18:04 +0530

    initial commit

--- node_modules/@storybook/addon-centered/angular.d.ts
+++
@@ -1,6 +0,0 @@
-// tslint:disable-next-line:no-implicit-dependencies
-import { IStory } from '@storybook/angular';
-
-declare module '@storybook/addon-centered/angular' {
-  export function centered(story: IStory): IStory;
-}
